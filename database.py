111e0a6cfa06b61aea315e802b904cd70ab58a88
jdszekeres <amyc21@yahoo.com>
2022-06-04 17:10:49 -0700

    /kəˈmit/

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
 url = "postgres://wxvjdiftesdjlv:a0a5e931304fb58d774c2ab7e42bbbb5840337d0192335be1993c49b1ba99635@ec2-54-165-178-178.compute-1.amazonaws.com:5432/d2dgk4gg48g0gr"
-WATTS_PER_HOUR = 1323
+WATTS_PER_HOUR = 1.323
 import psycopg2
 import os
 def cc():
