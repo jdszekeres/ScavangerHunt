ddc4c50518463aa9c7a299b1eaa47e89754f3bea
jdszekeres <amyc21@yahoo.com>
2022-06-13 15:43:02 -0700

    /kəˈmit/

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 URL = "https://api.geoapify.com/v2/places?categories=entertainment,leisure,tourism,beach,heritage&filter=circle:{lon},{lat},{meters}&bias=proximity:{lon},{lat}&limit={limit}&apiKey=276a11b14fef44f08a21535795486491"
 from flask import *
+from flask_compress import Compress
 import requests
 import database 
 badges = [
-    ("https://img.shields.io/badge/made-with-brightgreen","#","made with"),
-    ("https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white","https://github.com/jdszekeres/ScavangerHunt", "github"),
-    ("https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white", "https://www.postgresql.org/", "postgresql"),
-    ("https://img.shields.io/badge/heroku-%23430098.svg?style=for-the-badge&logo=heroku&logoColor=white", "https://heroku.com", "heroku"),
-    ("https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white","https://code.visualstudio.com/", "visual studios code"),
-    ("https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54","https://python.org", "python")
+    # ("https://img.shields.io/badge/made-with-brightgreen","#","made with"),
+    # ("https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white","https://github.com/jdszekeres/ScavangerHunt", "github"),
+    # ("https://img.shields.io/badge/postgres-%23316192.svg?style=for-the-badge&logo=postgresql&logoColor=white", "https://www.postgresql.org/", "postgresql"),
+    # ("https://img.shields.io/badge/heroku-%23430098.svg?style=for-the-badge&logo=heroku&logoColor=white", "https://heroku.com", "heroku"),
+    # ("https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white","https://code.visualstudio.com/", "visual studios code"),
+    # ("https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54","https://python.org", "python")
     ]
 tips = [
     'clicking the <i class="fa-solid fa-map-location-dot"></i> button will update your location',
@@ -18,7 +19,15 @@ tips = [
     ]
 
 app = Flask(__name__)
+Compress(app)
 app.secret_key = "\xb8\xb0spa07\x1c\xe0\xdb\xb9\xbaB\xb2\xa1\x92"
+def get_place(user,board):
+    for i,v in enumerate(board):
+        if user==v[1]:
+            return i+1
+app.jinja_env.globals.update(place=get_place)
+app.jinja_env.globals.update(enumerate=enumerate)
+app.jinja_env.globals.update(round=round)
 @app.after_request
 def enforce_https(response):
     response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains; preload"
@@ -26,7 +35,7 @@ def enforce_https(response):
 def get_lat_lon(ip):
     if ip == "127.0.0.1":
         ip = ""
-    req =requests.get("http://ip-api.com/json/"+ip+"?fields=lat,lon").json()
+    req =requests.get("http://ip-api.com/json/"+ip+"?fields=lat,lon", verify=False).json()
     return req["lat"], req["lon"]
 @app.route('/')
 def index():
@@ -38,7 +47,7 @@ def index():
     if "location" in session:
         lat = session["location"]["lat"]
         lon = session["location"]["lon"]
-    return render_template(
+    temp = make_response(render_template(
         'index.html',
         stuff=requests.get(
             URL.format(
@@ -46,7 +55,7 @@ def index():
                 lat=lat,
                 meters=meters,
                 limit=70
-                )
+                ), verify=False
             ).json()["features"],
         lat=lat,
         lon=lon,
@@ -54,7 +63,9 @@ def index():
         badges=badges,
         points=database.get_points(session["user"]),
         tip=str("&nbsp;"*70).join(tips)
-        )
+        ))
+    temp.headers.set("Content-Security-Policy","script-src 'nonce-1f40e4a23493'")
+    return temp
 @app.route("/signup", methods=["POST","GET"])
 def signup():
     if request.method == "POST":
@@ -87,5 +98,6 @@ def update_location():
     if "lat" in request.json:
         session["location"] = request.json
     return jsonify(request.json)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=1234,debug=True)
