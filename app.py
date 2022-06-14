7d59214fa9dabd457d36c63cae5228917a422a97
jdszekeres <amyc21@yahoo.com>
2022-06-14 12:50:49 -0700

    /kəˈmit/

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-URL = "https://api.geoapify.com/v2/places?categories=entertainment,leisure,tourism,beach,heritage&filter=circle:{lon},{lat},{meters}&bias=proximity:{lon},{lat}&limit={limit}&apiKey=276a11b14fef44f08a21535795486491"
+URL = "https://api.geoapify.com/v2/places?categories=entertainment,leisure,natural,national_park,pet.dog_park,tourism,beach,heritage,public_transport,ski,sport.stadium,building.historic,building.tourism,building.entertainment,activity&filter=circle:{lon},{lat},{meters}&bias=proximity:{lon},{lat}&limit={limit}&apiKey=276a11b14fef44f08a21535795486491"
 from flask import *
 from flask_compress import Compress
 import requests
 import database 
+from random import choice
 badges = [
     # ("https://img.shields.io/badge/made-with-brightgreen","#","made with"),
     # ("https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white","https://github.com/jdszekeres/ScavangerHunt", "github"),
@@ -11,12 +12,11 @@ badges = [
     # ("https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white","https://code.visualstudio.com/", "visual studios code"),
     # ("https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54","https://python.org", "python")
     ]
-tips = [
-    'clicking the <i class="fa-solid fa-map-location-dot"></i> button will update your location',
-    'until you click the <i class="fa-solid fa-map-location-dot"></i> button we use a approximate location that can be innacurate',
-    'clicking the <img src="/static/battery.png" alt="credits"> button will show the leaderboard',
-    'you can get directions to a marker by clicking on the marker and then cliciking the address'
+urls=[
+    "https://api.mapbox.com/styles/v1/yopo/cl3yq1zr3000014oaaez3upb0/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoieW9wbyIsImEiOiJjbDA0bzhoM2EwMWhiM2NxajV2Zm1lYmpyIn0.kL4KlQH8tl89C6dJtL31gw",
+    "https://api.mapbox.com/styles/v1/puggle-wuggle/cl4eig5ow000115oxhw88uiem/tiles/512/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoicHVnZ2xlLXd1Z2dsZSIsImEiOiJjbDRlaWVzMmswM3k5M2VsaWNpc2V3MGQ4In0.SC8EX6RjDonfDF8yo2Wjdw"
     ]
+tips = []
 
 app = Flask(__name__)
 Compress(app)
@@ -43,10 +43,17 @@ def index():
         return redirect("/login")
     miles = 10
     meters = miles*1610
+    limit=200
     lat, lon = get_lat_lon(request.headers.get('X-Forwarded-For',request.remote_addr))
     if "location" in session:
         lat = session["location"]["lat"]
         lon = session["location"]["lon"]
+    print(URL.format(
+                lon=lon,
+                lat=lat,
+                meters=meters,
+                limit=limit
+                ))
     temp = make_response(render_template(
         'index.html',
         stuff=requests.get(
@@ -54,7 +61,7 @@ def index():
                 lon=lon,
                 lat=lat,
                 meters=meters,
-                limit=70
+                limit=limit
                 ), verify=False
             ).json()["features"],
         lat=lat,
@@ -62,20 +69,27 @@ def index():
         radius=meters,
         badges=badges,
         points=database.get_points(session["user"]),
-        tip=str("&nbsp;"*70).join(tips)
+        tip=str("&nbsp;"*70).join(tips),
+        url=choice(urls)
         ))
     temp.headers.set("Content-Security-Policy","script-src 'nonce-1f40e4a23493'")
     return temp
 @app.route("/signup", methods=["POST","GET"])
 def signup():
     if request.method == "POST":
+        print(request.form)
         try:
             database.create_user(request.form.get("username"), request.form.get("password"))
             session["user"] = request.form.get("username")
             return redirect("/")
         except:
             return "username exists"
-    return render_template("term.html")
+    keywords = ['Mobile','Opera Mini','Android', 'Iphone', "Ipad"]
+    user_agent = request.headers.get('User-Agent').lower()
+    if any(word.lower() in user_agent for word in keywords):
+        return render_template("simple_signup.html")
+    else:
+        return render_template("term.html")
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
